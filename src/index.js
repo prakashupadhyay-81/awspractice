00a5118a9860f0ee022c743c2146af7015c34453
Prakash Upadhyay <prakash@PrakashMacBook.local>
2021-08-08 17:32:31 -0500

    test

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 // STARTS HERE
-import * as AWS from 'aws-sdk'
+import {ConfigurationOptions} from 'aws-sdk';
 
-const configuration = {
+const configuration: ConfigurationOptions = {
   region: 'Ohio',
   secretAccessKey: 'KMf1SNphL3AfAEwkg45kiV7wA0S9XciZ8iblBRzo',
   accessKeyId: 'AKIAV3IZ7OVN3VYX3UD7'
