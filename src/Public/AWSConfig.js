9e17f20b0f8acf05af0171038642926d831517e2
Prakash Upadhyay <prakash@PrakashMacBook.local>
2021-08-09 18:52:39 -0500

    test

--- src/Public/AWSConfig.js
+++ src/Public/AWSConfig.js
@@ -2,28 +2,11 @@ import * as AWS from 'aws-sdk'
 
 const docClient = new AWS.DynamoDB.DocumentClient({region: "us-east-1"})
 
+
 export const fetchData = (tableName) => {
-    console.log("Processing...");
-    const params = {
-        Item: {
-            date: Date.now(),
-            message: event.key1
-        },
-        TableName: "user"
-    };
-    const response = {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true,
-    },
-    body: JSON.stringify('Hello from new Lambda!'),
-  };
-    
-// export const fetchData = (tableName) => {
-//     var params = {
-//         TableName: tableName
-//     }
+    var params = {
+        TableName: tableName
+    }
 
     docClient.get(params, function (err, data) {
         if (!err) {
