888a3f12fc1eb5aa11ffaa8bd6cae70a738c12ec
Prakash Upadhyay <prakash@PrakashMacBook.local>
2021-08-09 20:52:47 -0500

    test

--- src/Public/AWSConfig.js
+++ src/Public/AWSConfig.js
@@ -3,50 +3,17 @@ import * as AWS from 'aws-sdk'
 const docClient = new AWS.DynamoDB.DocumentClient({region: "us-east-1",secretAccessKey: 'KMf1SNphL3AfAEwkg45kiV7wA0S9XciZ8iblBRzo',
 accessKeyId: 'AKIAV3IZ7OVN3VYX3UD7'})
 
-
 export const fetchData = (tableName) => {
-    console.log("Processing...");
-           var params = {
-            TableName: tableName
-        }
-    const response = {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true,
-    },
-    body: JSON.stringify('Hello from Lambda function'),
-  };
+    var params = {
+        TableName: tableName
+    }
 
-    docClient.get(params, response, function(err, data) {
+    docClient.scan(params, function (err, data) {
         if(err){
-            console.log(err);
-        } else {
-            console.log(data);
+            console.log(err)
         }
-    });
-};
-
-// export const fetchData = (tableName) => {
-//     var params = {
-//         TableName: tableName
-//     }
-
-//     const response = {
-//         statusCode: 200,
-//         headers: {
-//           'Access-Control-Allow-Origin': '*',
-//           'Access-Control-Allow-Credentials': true,
-//         },
-//         body: JSON.stringify('Hello from Lambda function'),
-//     }
-
-//     docClient.query(params, function (err, data) {
-//         if(err){
-//             console.log(err)
-//         }
-//         if (!err) {
-//             console.log(data)
-//         }
-//     })
-// }
+        if (!err) {
+            console.log(data)
+        }
+    })
+}
