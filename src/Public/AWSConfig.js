2888a869b3ea9e6b7e169219db7b5d74a02b381b
Prakash Upadhyay <prakash@PrakashMacBook.local>
2021-08-10 08:30:46 -0500

    test

--- src/Public/AWSConfig.js
+++ src/Public/AWSConfig.js
@@ -1,6 +1,6 @@
 import * as AWS from 'aws-sdk'
 
-const docClient = new AWS.DynamoDB.DocumentClient({region: "us-east-1",secretAccessKey: 'KMf1SNphL3AfAEwkg45kiV7wA0S9XciZ8iblBRzo',
+const docClient = new AWS.DynamoDB.DocumentClient({region: "us-east-2",secretAccessKey: 'KMf1SNphL3AfAEwkg45kiV7wA0S9XciZ8iblBRzo',
 accessKeyId: 'AKIAV3IZ7OVN3VYX3UD7'})
 
 export const fetchData = (tableName) => {
